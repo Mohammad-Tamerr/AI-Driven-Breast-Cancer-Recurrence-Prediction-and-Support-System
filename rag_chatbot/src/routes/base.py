22b13eaf468c5b6da880d3b3eed265cb4ca2beed
Mohammad-Tamerr <mohammadtamer1212@gmail.com>
2025-12-22 01:56:53 +0200

    Edit main route

--- rag_chatbot/src/routes/base.py
+++ rag_chatbot/src/routes/base.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, APIRouter, Depends
 import os
 
 base_router = APIRouter(
-    prefix="/api/v1",
-    tags=["api_v1"],
+    prefix="/Rafeek/v1",
+    tags=["Rafeek_v1"],
 )
 
 @base_router.get("/")
