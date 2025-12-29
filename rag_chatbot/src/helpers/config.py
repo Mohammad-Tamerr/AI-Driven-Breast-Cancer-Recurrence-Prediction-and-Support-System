b3af7308fe085a54ebfd5b2b655a59a364f3ee72
belalmohse13 <belalmohsen113@gmail.com>
2025-12-30 00:45:19 +0200

    update configuration

--- rag_chatbot/src/helpers/config.py
+++ rag_chatbot/src/helpers/config.py
@@ -9,6 +9,18 @@ class Settings(BaseSettings):
     MAX_FILE_SIZE_MB: int
     CHUNK_SIZE: int
 
+    OPENAI_API_KEY: str = None
+    OPENAI_API_URL: str = None
+    COHERE_API_KEY: str = None
+
+    GENERATION_MODEL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+    INPUT_DAFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DAFAULT_MAX_TOKENS: int = None
+    GENERATION_DAFAULT_TEMPERATURE: float = None
+
+
     class Config:
         env_file = ".env"
 
