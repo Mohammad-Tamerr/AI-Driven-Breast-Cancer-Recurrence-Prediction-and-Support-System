fd7d5d188116d96187de7a0a11fc4497fefeb2f7
belalmohse13 <belalmohsen113@gmail.com>
2025-12-30 23:30:03 +0200

    testing

--- rag_chatbot/src/helpers/config.py
+++ rag_chatbot/src/helpers/config.py
@@ -3,11 +3,16 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
-    OPENAI_API_KEY: str
-
+    
     FILE_ALLOWED_TYPES: list 
     MAX_FILE_SIZE_MB: int
     CHUNK_SIZE: int
+
+    MONGODB_URL: str = None
+    MONGODB_DATABASE: str = None
+
+    GENERATION_BACKEND: str = None
+    EMBEDDING_BACKEND: str = None
 
     OPENAI_API_KEY: str = None
     OPENAI_API_URL: str = None
@@ -19,7 +24,6 @@ class Settings(BaseSettings):
     INPUT_DAFAULT_MAX_CHARACTERS: int = None
     GENERATION_DAFAULT_MAX_TOKENS: int = None
     GENERATION_DAFAULT_TEMPERATURE: float = None
-
 
     class Config:
         env_file = ".env"
