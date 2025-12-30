c967537fe08e98d4e49a7e04f142da1a53479c8e
belalmohse13 <belalmohsen113@gmail.com>
2025-12-30 22:05:20 +0200

    fixing the first abtractmethod

--- rag_chatbot/stores/LLM/LLMInterface.py
+++ rag_chatbot/stores/LLM/LLMInterface.py
@@ -2,13 +2,13 @@ from abc import ABC, abstractmethod
 class LLMInterface(ABC):
 
     @abstractmethod
-    def set_generate_model(self, model_id: str): 
+    def set_generation_model(self, model_id: str): 
         pass # Set the model for text generation (1e.g., GPT-4, PaLM2)
 
 
     @abstractmethod
     def set_embedding_model(self, model_id: str, embedding_size: int):
-        pass # Set the model for text embedding (1e.g., Ada-002, E5-Large)
+        pass # Set the model for text embedding (e.g., Ada-002, E5-Large)
 
     @abstractmethod
     def generate_text(self, prompt: str, chat_history: list=[], max_output_tokens: int=None,
